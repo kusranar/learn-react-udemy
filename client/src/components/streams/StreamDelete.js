737663667815d9e22514e4cb888bd1920de6c53c
kusrana <rizki.kusrana@gmail.com>
2020-05-02 23:01:16 +0700

    Using React Portals

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Modal from '../Modal';
+import history from '../../history';
+import { fetchStream, deleteStream } from '../../redux/actions';
 
-const StreamDelete = () => {
-    return (
-        <div>
-            StreamDelete
-        </div>
-    )
+class StreamDelete extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    renderActions = () => {
+        const { id } = this.props.match.params;
+        return (
+            <React.Fragment>
+                <button
+                    className="ui button negative"
+                    onClick={() => this.props.deleteStream(id)}
+                >
+                    Delete
+                </button>
+                <Link
+                    to="/"
+                    className="ui button"
+                >
+                    Cancel
+                </Link>
+            </React.Fragment>
+        )
+    }
+
+    renderContent = () => {
+        if (!this.props.stream) {
+            return 'Are you sure want to delete this stream?';
+        }
+
+        return `Are you sure want to delete this stream with title : ${this.props.stream.title}?`;
+    }
+
+    render() {
+        return <Modal
+            title="Delete Stream"
+            content={this.renderContent()}
+            actions={this.renderActions()}
+            onDismiss={() => history.push('/')}
+        />
+    }
 }
 
-export default StreamDelete;
+const mapStateToPorps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToPorps, { fetchStream, deleteStream })(StreamDelete);
--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -64,4 +64,5 @@ export const deleteStream = streamId => async dispatch => {
         type: DELETE_STREAM,
         payload: streamId
     });
+    history.push('/');
 }
